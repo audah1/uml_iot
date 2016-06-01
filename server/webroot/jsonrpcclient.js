233ef57f40ba99c9e11db71c0b84a004f00a1646
unknown <>
2016-06-01 13:31:00 +0900

    jsonrpcclient

--- server/webroot/jsonrpcclient.js
+++ server/webroot/jsonrpcclient.js
@@ -9,8 +9,106 @@ function sleep(num){	//[1/1000초]
     }
 }
 
+var callbackTable={}; //table을 만들어 각 id에 부여된 값을 이용해 callback 함수를 불러준다.
+var callbackTable_rsvd = {};
+
+function M_Callback(id,res)
+{
+    var fcn = callbackTable[id];
+    var rsvd = callbackTable_rsvd[id];
+    callbackTable[id] = undefined;   ///////부여된 id값 삭제, callback 수행되기전에.
+    callbackTable_rsvd[id] = undefined;
+    if(fcn == undefined){
+        console.log('Non callback : ' , id, res);
+        return -1;
+    }else{
+        return fcn(rsvd, res);
+    }
+}
+
+function HandlingJSONres(responseobject){
+        if(responseobject.length==undefined)M_Callback(responseobject.id, responseobject.RES);                   
+        else{
+            var results=[];
+            for(var i=0; i<responseobject.length; i++)
+            {
+                results[i]=responseobject[i].RES;
+            }
+            M_Callback(responseobject[0].id, results);
+        }
+}
+
+function getRPC(connName, port, fcnCallback){
+
+    var idGererate = 1;
+    function writeObject(socket, requestobject_or_objectTable, compCallbackin, rsvd, writeLineInput)
+    { //compCallbackin 대신 cb table?
+        if(writeLineInput==undefined){
+            writeLineInput=writeLine;
+        }
+        var requestobject,requestobjects;
+        if(requestobject_or_objectTable.length==undefined){
+            requestobjects=requestobject_or_objectTable;
+        }
+        else{ 
+            requestobject=requestobject_or_objectTable[0];
+        }
+        requestobject.id = idGererate++;
+        var id = requestobject.id;
+        callbackTable[id]= compCallbackin;
+        callbackTable_rsvd[id]= rsvd;
+        writeLineInput(socket,JSON.stringify(requestobjects));
+    };
+    function makeObject1(requestobject_or_objectTable, compCallbackin, rsvd)
+    { //compCallbackin 대신 cb table?
+        var requestobject,requestobjects;
+        if(requestobject_or_objectTable.length==undefined){ //객체라는거
+            requestobjects=requestobject_or_objectTable; 
+        }
+        else{ //여러개
+            requestobject=requestobject_or_objectTable[0];
+        }
+        requestobjects.id = idGererate++;
+        var id = requestobjects.id;
+        callbackTable[id]= compCallbackin;
+        callbackTable_rsvd[id]= rsvd;
+
+        //writeLine(socket,JSON.stringify(requestobjects));
+        return requestobjects||requestobject;
+
+    };
+    function makeObject(requestobject_or_objectTable, compCallbackin, rsvd)
+    { //compCallbackin 대신 cb table?
+        var requestobject,requestobjects;
+        if(requestobject_or_objectTable.length==undefined){ //객체라는거
+            requestobject=requestobject_or_objectTable; 
+        }
+        else{ //여러개
+            requestobject=requestobject_or_objectTable[0];
+            requestobjects=requestobject_or_objectTable;
+        }
+        requestobject.id = idGererate++;
+        var id = requestobject.id;
+        callbackTable[id]= compCallbackin;
+        callbackTable_rsvd[id]= rsvd;
+
+        //writeLine(socket,JSON.stringify(requestobjects));
+        return requestobjects||requestobject;
+
+    };
+    return {/*client:client,*/ writeObject:writeObject, makeObject:makeObject};
+}
+
+exports.rpc=getRPC;
+//exports.HandlingJSONresult=HandlingJSONresult;
+exports.HandlingJSONres=HandlingJSONres;
+
+});
+
+/*
+
 //var fcnCallback0 = function(res){console.log("Oper Result == ", res);};
-/*function writeData(socket, datain){
+function writeData(socket, datain){
     var data = datain;
     var success = !socket.write(data);
     if (!success){/*
@@ -102,24 +200,7 @@ var getConnection = function(connName, port, fcnCallback){
   });
   return client;
 };
-*/
-var callbackTable={}; //table을 만들어 각 id에 부여된 값을 이용해 callback 함수를 불러준다.
-var callbackTable_rsvd = {};
 
-function M_Callback(id,res)
-{
-    var fcn = callbackTable[id];
-    var rsvd = callbackTable_rsvd[id];
-    callbackTable[id] = undefined;   ///////부여된 id값 삭제, callback 수행되기전에.
-    callbackTable_rsvd[id] = undefined;
-    if(fcn == undefined){
-        console.log('Non callback : ' , id, res);
-        return -1;
-    }else{
-        return fcn(rsvd, res);
-    }
-}
-/*
 var buffereddata=""; //not multiple use;
 function HandlingJSONresult(data){
     data=buffereddata+data;
@@ -153,83 +234,5 @@ function HandlingJSONresult(data){
     }
 }
 */
-function HandlingJSONres(responseobject){
-        if(responseobject.length==undefined)M_Callback(responseobject.id, responseobject.RES);                   
-        else{
-            var results=[];
-            for(var i=0; i<responseobject.length; i++)
-            {
-                results[i]=responseobject[i].RES;
-            }
-            M_Callback(responseobject[0].id, results);
-        }
-}
-
-function getRPC(connName, port, fcnCallback){
-
-    var idGererate = 1;
-    function writeObject(socket, requestobject_or_objectTable, compCallbackin, rsvd, writeLineInput)
-    { //compCallbackin 대신 cb table?
-        if(writeLineInput==undefined){
-            writeLineInput=writeLine;
-        }
-        var requestobject,requestobjects;
-        if(requestobject_or_objectTable.length==undefined){
-            requestobjects=requestobject_or_objectTable;
-        }
-        else{ 
-            requestobject=requestobject_or_objectTable[0];
-        }
-        requestobject.id = idGererate++;
-        var id = requestobject.id;
-        callbackTable[id]= compCallbackin;
-        callbackTable_rsvd[id]= rsvd;
-        writeLineInput(socket,JSON.stringify(requestobjects));
-    };
-    function makeObject1(requestobject_or_objectTable, compCallbackin, rsvd)
-    { //compCallbackin 대신 cb table?
-        var requestobject,requestobjects;
-        if(requestobject_or_objectTable.length==undefined){ //객체라는거
-            requestobjects=requestobject_or_objectTable; 
-        }
-        else{ //여러개
-            requestobject=requestobject_or_objectTable[0];
-        }
-        requestobjects.id = idGererate++;
-        var id = requestobjects.id;
-        callbackTable[id]= compCallbackin;
-        callbackTable_rsvd[id]= rsvd;
-
-        //writeLine(socket,JSON.stringify(requestobjects));
-        return requestobjects||requestobject;
-
-    };
-    function makeObject(requestobject_or_objectTable, compCallbackin, rsvd)
-    { //compCallbackin 대신 cb table?
-        var requestobject,requestobjects;
-        if(requestobject_or_objectTable.length==undefined){ //객체라는거
-            requestobject=requestobject_or_objectTable; 
-        }
-        else{ //여러개
-            requestobject=requestobject_or_objectTable[0];
-            requestobjects=requestobject_or_objectTable;
-        }
-        requestobject.id = idGererate++;
-        var id = requestobject.id;
-        callbackTable[id]= compCallbackin;
-        callbackTable_rsvd[id]= rsvd;
-
-        //writeLine(socket,JSON.stringify(requestobjects));
-        return requestobjects||requestobject;
-
-    };
-    return {/*client:client,*/ writeObject:writeObject, makeObject:makeObject};
-}
-
-exports.rpc=getRPC;
-//exports.HandlingJSONresult=HandlingJSONresult;
-exports.HandlingJSONres=HandlingJSONres;
-
-});
 
 
