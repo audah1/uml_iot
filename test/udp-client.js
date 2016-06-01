c0bfbabaeb7726993e38d9bb131daaf64833c931
unknown <>
2016-06-01 10:02:38 +0900

    udp client

--- test/udp-client.js
+++ test/udp-client.js
@@ -7,7 +7,7 @@ var async = require("async");
 var readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
 
-var xml2js=require('../server/public/xml2js/lib/xml2js');
+var xml2js=require('../node_modules/xml2js/lib/xml2js');
 var xmlparser=new xml2js.Parser();
 var builder = new xml2js.Builder();
 var getRPC =jsonclient.rpc;
